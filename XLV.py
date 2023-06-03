98c5a530d0b86480a1d92cc492cde0e79f88f0e7
Schryzon <myuuko92@gmail.com>
2023-06-04 07:01:16 +0800

    Fixed XLV.py even if its not needed.

--- XLV.py
+++ XLV.py
@@ -2,40 +2,50 @@
 X-LV
 Xtreme Log-out Vigilante
 Used to detect RVDIA's status
+WARNING: This is only a replica of what was written in replit.
+         But feel free to configure it though.
 """
 
 import discord
 from os import getenv
-from dotenv import load_dotenv
-from RVDIA import rvdia
 from discord.ext import commands
-load_dotenv('./secrets.env')
+# from keep_alive import keep_alive
 
-xlv = commands.Bot(command_prefix="x-", help_command=None, intents=discord.Intents.all(), status=discord.Game('and monitoring RVDIA...'))
+# keep_alive()
+
+xlv = commands.Bot(command_prefix="x-",
+                   help_command=None,
+                   intents=discord.Intents.all(),
+                   activity=discord.Activity(type=discord.ActivityType.watching, name="RVDiA"))
+
 
 @xlv.event
 async def on_connect():
-    print('XLV connected!')
+  print('XLV connected!')
+
 
 @xlv.event
 async def on_ready():
-    await xlv.wait_until_ready()
-    print('XLV is ready!')
+  await xlv.wait_until_ready()
+  print('XLV is ready!')
+
 
 @xlv.event
 async def on_presence_update(before, after):
-    if before.id == rvdia.user.id and after.id == rvdia.user.id:
-        channel = xlv.get_channel(883188578245562378)
-        if before.status == "idle" and after.status == "offline" or after.status == "invisible":
-            await channel.send(f"<@{xlv.owner_id}>\n⚪ RVDIA is now **`OFFLINE`**!")
+  if before.id == 957471338577166417 and after.id == 957471338577166417:
+    channel = xlv.get_channel(883188578245562378)
+    if str(before.status) == "idle" and str(after.status) == "offline" or str(after.status) == "invisible":
+      await channel.send("<@877008612021661726>\n⚪ RVDIA is now **`OFFLINE`**!")
 
-        else:
-            await channel.send(f"<@{xlv.owner_id}>\n🟡 RVDIA is back **`ONLINE`**!")
-        
-    return
+    elif str(before.status) == "offline" or str(before.status) == "invisible" and str(after.status) == "idle" or str(after.status) == 'online':
+      await channel.send("<@877008612021661726>\n🟡 RVDIA is back **`ONLINE`**!")
+
+  return
+
 
 @xlv.event
 async def on_message(message):
-    return # This bot doesn't need commands, just tell.
+  return  # This bot doesn't need commands, just tell.
+
 
 xlv.run(getenv('xlvtoken'))
